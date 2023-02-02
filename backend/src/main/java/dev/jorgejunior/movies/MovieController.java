dea95338bf261ee0b606bec447f0753df224a41c
Jorge jr <jorgesilvajunior10@gmail.com>
2023-02-02 13:44:32 -0300

    first commit: structured project

---
+++ backend/src/main/java/dev/jorgejunior/movies/MovieController.java
@@ -0,0 +1,33 @@
+package dev.jorgejunior.movies;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+	
+	@Autowired
+    private MovieService movieService; 
+	
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> getMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getMovieByID(@PathVariable String id ){
+    	 
+    	return new ResponseEntity<Optional<Movie>>(movieService.getMovieByID(id), HttpStatus.OK);
+    }
+
+}
