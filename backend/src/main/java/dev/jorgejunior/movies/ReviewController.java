428a172aa001bc89316903b2271042d1a06ec7d5
Jorge jr <jorgesilvajunior10@gmail.com>
2023-02-03 12:00:51 -0300

    fix cors: add cross origin

--- backend/src/main/java/dev/jorgejunior/movies/ReviewController.java
+++ backend/src/main/java/dev/jorgejunior/movies/ReviewController.java
@@ -5,11 +5,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@CrossOrigin(origins = {"http://localhost:3000"})
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
